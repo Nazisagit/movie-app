f953be1c4134200eefe3d09b34a4f916ca2a2ce3
Nazisagit <pgnazrin@gmail.com>
2024-06-30 18:21:54 +0800

    missing route changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  namespace :api, defaults: { format: :json } do
+    resources :movies, only: :index
+  end
+  resources :movies, only: :index do
+    resources :reviews, only: :index
+  end
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "movies#index"
 end
